6cedb53714477dfc8af32d57ccf8bbd3203448d7
Jarred Glaser <32422167+jdglaser@users.noreply.github.com>
2020-08-06 22:17:45 -0500

    Delete deploy.bat

--- scripts/deploy.bat
+++
@@ -1,2 +0,0 @@
-zip -r ../deployment.zip .
-aws lambda update-function-code --function-name arn:aws:lambda:us-east-1:618556235307:function:RepScrape --zip-file fileb://C:/Users/jarre/Documents/deployment.zip
